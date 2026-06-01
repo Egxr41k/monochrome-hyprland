0c281ec9672cad1646ad4744dd6090d68afe7611
egxr41k <egor2005krava@gmail.com>
2026-06-01 19:14:49 +0300

    update install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+# hyprland setup
+
+# terminal setup
+
+# soft
+
+
+# paru
+sudo pacman -Syu
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si < \n
+
+
+paru -S peaclock
+sudo pacman -S cmatrix cava
+
+
+cp -r wallpapers ~/Pictures/Wallpapers
+sudo pacman -S neovim
+
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
